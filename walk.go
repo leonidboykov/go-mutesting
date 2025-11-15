6c5e36f9391ace8b726102b994f62b920498fe91
Leonid Boykov <leonidboykov@proton.me>
2025-11-15 10:19:49 +0200

    feat: cleanup walkers

--- walk.go
+++ walk.go
@@ -14,38 +14,28 @@ import (
 // and calls the method Check of the given mutator for every node and sums up the returned counts. After completion of
 // the traversal the final counter is returned.
 func CountWalk(pkg *packages.Package, node ast.Node, m mutator.Mutator, skippedLines map[int]struct{}) int {
-	w := &countWalk{
-		count:   0,
-		mutator: m,
-		pkg:     pkg,
-		skipped: skippedLines,
+	var count int
+	for n := range ast.Preorder(node) {
+		line := pkg.Fset.Position(node.Pos()).Line
+		if _, ok := skippedLines[line]; !ok {
+			count += len(m(pkg.Types, pkg.TypesInfo, n))
+		}
 	}
-
-	ast.Walk(w, node)
-
-	return w.count
+	return count
 }
 
-type countWalk struct {
-	count   int
-	mutator mutator.Mutator
-	pkg     *packages.Package
-	skipped map[int]struct{}
-}
-
-// Visit implements the Visit method of the ast.Visitor interface
-func (w *countWalk) Visit(node ast.Node) ast.Visitor {
-	if node == nil {
-		return w
-	}
-
-	line := w.pkg.Fset.Position(node.Pos()).Line
-	_, ok := w.skipped[line]
-	if !ok {
-		w.count += len(w.mutator(w.pkg.Types, w.pkg.TypesInfo, node))
-	}
-
-	return w
+// PrintWalk traverses the AST of the given node and prints every node to STDOUT.
+func PrintWalk(node ast.Node) {
+	var level int
+	ast.Inspect(node, func(node ast.Node) bool {
+		if node != nil {
+			level++
+			fmt.Printf("%s(%p)%#v\n", strings.Repeat("\t", level), node, node)
+		} else {
+			level--
+		}
+		return true
+	})
 }
 
 // MutateWalk mutates the given node with the given mutator returning a channel to control the mutation steps. It
@@ -96,32 +86,6 @@ func (w *mutateWalk) Visit(node ast.Node) ast.Visitor {
 		m.Reset()
 		w.changed <- true
 		<-w.changed
-	}
-
-	return w
-}
-
-// PrintWalk traverses the AST of the given node and prints every node to STDOUT.
-func PrintWalk(node ast.Node) {
-	w := &printWalk{
-		level: 0,
-	}
-
-	ast.Walk(w, node)
-}
-
-type printWalk struct {
-	level int
-}
-
-// Visit implements the Visit method of the ast.Visitor interface
-func (w *printWalk) Visit(node ast.Node) ast.Visitor {
-	if node != nil {
-		w.level++
-
-		fmt.Printf("%s(%p)%#v\n", strings.Repeat("\t", w.level), node, node)
-	} else {
-		w.level--
 	}
 
 	return w
